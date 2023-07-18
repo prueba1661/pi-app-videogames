597a22293610b2926e3652056ea20020b1f394ec
Kary1661 <posposkarina@gmail.com>
2023-07-18 11:20:26 -0300

    last

--- client/jest.config.js
+++ client/jest.config.js
@@ -4,5 +4,9 @@ module.exports = {
     transform: {
       '^.+\\.jsx?$': 'babel-jest',
     },
+
+    transformIgnorePatterns: [
+      '/node_modules/(?!nombre-del-paquete|otro-paquete)/',
+    ],
   };
   
