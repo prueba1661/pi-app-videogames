6ac4854b07d430858ee255d92c3476dab94b4f25
Karina Pospos <114627942+Kary1661@users.noreply.github.com>
2023-09-28 18:46:46 -0300

    Add files via upload

--- client/src/utils/Loading.jsx
+++ client/src/utils/Loading.jsx
@@ -1,13 +1,27 @@
+// import React from 'react'
+// import loading from '../assets/loading.gif'
+// import styles from './utils.module.css'
+
+// const Loading = () => (
+//   <div>
+//   <img src={loading} alt="loading" 
+//   className={styles.loading}
+//   />
+//   <p className={styles.loadingText}>Loading...</p>
+//   </div>
+// )
+
+// export default Loading
 import React from 'react'
-import loading from '../assets/loading.gif'
-import styles from './utils.module.css'
+import loading from '../assets/loading-neon.gif'
+import styles from './Loading.module.css'
 
 const Loading = () => (
-  <div>
+  <div className={styles.container}>
   <img src={loading} alt="loading" 
   className={styles.loading}
   />
-  <p className={styles.loadingText}>Loading...</p>
+  <p className={styles.loadingText}>We're loading...</p>
   </div>
 )
 
---
+++ client/src/utils/NotFound/NotFound.jsx
@@ -0,0 +1,18 @@
+import style from '../NotFound/NotFound.module.css';
+import {NavLink} from 'react-router-dom';
+import glitch from '../../assets/glitch-404.gif'
+
+const NotFound = () => {
+    return(
+        <div className={style.container}>
+                <img src={glitch}
+                alt="not found"
+                className={style.img}
+                />
+                <p className={style.text}>Oops! Page not found</p>
+                <NavLink to='/'><button className={style.btn}>Back</button></NavLink>
+            </div>
+    )
+}
+
+export default NotFound;
