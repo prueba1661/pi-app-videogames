c508ddc3272d9655bba9fe62d3086858721133ea
Kary1661 <posposkarina@gmail.com>
2023-09-29 14:10:35 -0300

    vercel url

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Origin', 'https://app-videogames-26cunh4mw-kary1661.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   if ('OPTIONS' == req.method) {
